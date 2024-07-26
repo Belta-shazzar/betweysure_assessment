a3f80f9e538ba518260e8d7c9e2a5ebb7c8c2fd1
Daniel Oguejiofor <nielnonso@gmail.com>
2024-07-26 13:18:35 +0100

    feat: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger();
@@ -14,6 +15,15 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('BWS Swagger Documnetation')
+    .setDescription('API Documenntation for BetWeySure Assesment')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-doc', app, document);
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
